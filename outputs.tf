8c7b043ae145f7d724fe88c3951270a6723f5251
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-02-27 12:58:02 +1100

    Updated projects

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,23 @@
-output "Project_A_Id" {
-  value = tfe_project.AppA.id
+output "project_hcp_boundary_id" {
+  value = tfe_project.hcp_boundary
+}
+
+output "project_hcp_consul_id" {
+  value = tfe_project.hcp_consul
+}
+
+output "project_hcp_packer_id" {
+  value = tfe_project.hcp_packer
+}
+
+output "project_hcp_vault_id" {
+  value = tfe_project.hcp_vault
+}
+
+output "project_hcp_waypoint_id" {
+  value = tfe_project.hcp_waypoint
+}
+
+output "project_tfc_repo_demo_id" {
+  value = tfe_project.tfc_repo_demo
 }
--- projects.tf
+++ projects.tf
@@ -1,4 +1,29 @@
-resource "tfe_project" "AppA" {
+resource "tfe_project" "hcp_vault" {
   organization = var.orgName
-  name         = "Application A"
+  name         = "HCP Vault"
+}
+
+resource "tfe_project" "hcp_consul" {
+  organization = var.orgName
+  name         = "HCP Consul"
+}
+
+resource "tfe_project" "hcp_waypoint" {
+  organization = var.orgName
+  name         = "HCP Waypoint"
+}
+
+resource "tfe_project" "hcp_boundary" {
+  organization = var.orgName
+  name         = "HCP Boundary"
+}
+
+resource "tfe_project" "hcp_packer" {
+  organization = var.orgName
+  name         = "HCP Packer"
+}
+
+resource "tfe_project" "tfc_repo_demo" {
+  organization = var.orgName
+  name         = "TFC Repo Demo"
 }
