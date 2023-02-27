96e329fdce26fd63d9619db89aed34aab79ea6a0
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-02-27 13:19:44 +1100

    Edited output

--- outputs.tf
+++ outputs.tf
@@ -1,23 +1,23 @@
-output "project_hcp_boundary_id" {
+output "project_hcp_boundary" {
   value = tfe_project.hcp_boundary
 }
 
-output "project_hcp_consul_id" {
+output "project_hcp_consul" {
   value = tfe_project.hcp_consul
 }
 
-output "project_hcp_packer_id" {
+output "project_hcp_packer" {
   value = tfe_project.hcp_packer
 }
 
-output "project_hcp_vault_id" {
+output "project_hcp_vault" {
   value = tfe_project.hcp_vault
 }
 
-output "project_hcp_waypoint_id" {
+output "project_hcp_waypoint" {
   value = tfe_project.hcp_waypoint
 }
 
-output "project_tfc_repo_demo_id" {
+output "project_tfc_repo_demo" {
   value = tfe_project.tfc_repo_demo
 }
