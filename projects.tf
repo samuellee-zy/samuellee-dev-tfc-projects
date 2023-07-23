d2a723037dc18f722ea891d53699d9ec3f119c68
Samuel Lee <33992350+samuellee-zy@users.noreply.github.com>
2023-07-24 00:06:25 +1000

    removed unnecessary projects

--- projects.tf
+++ projects.tf
@@ -3,24 +3,24 @@ resource "tfe_project" "hcp_vault" {
   name         = "HCP Vault"
 }
 
-resource "tfe_project" "hcp_consul" {
-  organization = var.orgName
-  name         = "HCP Consul"
-}
+# resource "tfe_project" "hcp_consul" {
+#   organization = var.orgName
+#   name         = "HCP Consul"
+# }
 
-resource "tfe_project" "hcp_waypoint" {
-  organization = var.orgName
-  name         = "HCP Waypoint"
-}
+# resource "tfe_project" "hcp_waypoint" {
+#   organization = var.orgName
+#   name         = "HCP Waypoint"
+# }
 
-resource "tfe_project" "hcp_boundary" {
-  organization = var.orgName
-  name         = "HCP Boundary"
-}
+# resource "tfe_project" "hcp_boundary" {
+#   organization = var.orgName
+#   name         = "HCP Boundary"
+# }
 
-resource "tfe_project" "hcp_packer" {
-  organization = var.orgName
-  name         = "HCP Packer"
+# resource "tfe_project" "hcp_packer" {
+#   organization = var.orgName
+#   name         = "HCP Packer"
 }
 
 resource "tfe_project" "tfc_repo_demo" {
